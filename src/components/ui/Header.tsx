be75937ab735c6269685037d8cd818fc13105cda
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-01 18:39:40 +0330

    feat: create header
    Des:
    create header components
    - add dialog + avatar components
    - use lucide-react icons
    - change style of button component ( add curser-pointer )

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,8 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./button";
+import { Github, LogOut, User } from "lucide-react";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./dialog";
 
 function Header({ children }: { children: React.ReactNode }) {
     return (
-        <header className="w-full">{children}</header>
+        <header className="w-full flex justify-between items-center md:p-3 bg-sidebar border-b border-sidebar-border shadow-sm shadow-sidebar">
+            <div>
+                {children}
+            </div>
+            <div className="flex items-center gap-1">
+                <div className="flex items-center gap-2">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <span>user name</span>
+                </div>
+                <Link to={'https://github.com/farhad-ahmadi-04/'} target="_blank">
+                    <Button size={"icon"} variant={"ghost"}><Github /></Button>
+                </Link>
+                <Link to={'account'}>
+                    <Button size={"icon"} variant={"ghost"}><User /></Button>
+                </Link>
+                <ModeToggle />
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button size={"icon"} variant={"ghost"}><LogOut /></Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-md">
+                        <DialogHeader>
+                            <DialogTitle>Are you absolutely sure?</DialogTitle>
+                            <DialogDescription>
+                                This action cannot be undone. This will permanently delete your account
+                                and remove your data from our servers.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter className="sm:justify-start">
+                            <DialogClose asChild>
+                                <Button>Cancel</Button>
+                            </DialogClose>
+                            <DialogClose asChild>
+                                <Button variant={"outline"}>Logout</Button>
+                            </DialogClose>
+
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+
+            </div>
+        </header>
     );
 }
 
--- src/components/ui/appLayout.tsx
+++ src/components/ui/appLayout.tsx
@@ -2,17 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "@/components/ui/Header";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar"
-import { ModeToggle } from "../mode-toggle";
+
 
 function AppLayout() {
     return (
         <>
             <SidebarProvider>
                 <AppSidebar />
-                <div className="w-full overflow-hidden">
+                <div className="w-full h-svh overflow-hidden">
                     <Header>
                         <SidebarTrigger />
-                        <ModeToggle />
                     </Header>
                     <main className="p-2 md:p-5 w-full flex justify-center items-center overflow-y-auto">
                         {/* container */}
