3c8d1024e87e8afe1e3c0f45b32c7c567e09a49a
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-04 17:51:37 +0330

    refactor(UI): title for cabins page

--- src/pages/cabins.tsx
+++ src/pages/cabins.tsx
@@ -1,5 +1,13 @@
+import Section from "@/components/ui/section";
+
 function Cabins() {
-    return (<h1>Cabins</h1>);
+    return (
+        <Section>
+            <div className="flex justify-between">
+                <h1 className="text-lg md:text-2xl lg:text-4xl font-semibold">Cabins</h1>
+            </div>
+        </Section >
+    );
 }
 
 export default Cabins;
