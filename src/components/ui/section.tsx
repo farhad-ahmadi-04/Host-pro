a3a16c320f2abd3df84e4cb0300c5af43b85e7c1
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-05 19:25:48 +0330

    refactor(component): add style
    Des:
    add gap to section

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Section({ className, children }:
     { className?: string, children: React.ReactNode }) {
     return (
-        <section className={cn("flex flex-col w-full", className)}>
+        <section className={cn("flex flex-col gap-2 md:gap-4 lg:gap-5 w-full", className)}>
             {children}
         </section>);
 }
