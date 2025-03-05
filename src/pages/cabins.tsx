49df7e7cf4c8a8f3756cb337e55076bdf35a5165
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-05 19:28:57 +0330

    refactor(component): add component
    Des:
    add CabinTable component to this component and handle UI and logic of table in another component

--- src/pages/cabins.tsx
+++ src/pages/cabins.tsx
@@ -1,41 +1,14 @@
 import Section from "@/components/ui/section";
-import { columns, Payment } from "@/components/ui/table/columns";
-import { DataTable } from "@/components/ui/table/data-table";
-import { useEffect, useState } from "react";
-
-async function getData(): Promise<Payment[]> {
-    // Fetch data from your API here.
-    return [
-        {
-            id: "728ed52f",
-            amount: 100,
-            status: "pending",
-            email: "m@example.com",
-        },
-        // ...
-    ]
-}
-
+import CabinTable from "@/features/cabinTable";
 
 function Cabins() {
-    const [data, setData] = useState<Payment[]>([])
-
-    useEffect(() => {
-        async function fetch() {
-            const data = await getData()
-            setData(data)
-        }
-        fetch()
-    }, [])
-
-
     return (
         <Section>
             <div className="flex justify-between">
                 <h1 className="text-lg md:text-2xl lg:text-4xl font-semibold">Cabins</h1>
             </div>
             <div>
-                <DataTable columns={columns} data={data} />
+                <CabinTable />
             </div>
         </Section >
     );
