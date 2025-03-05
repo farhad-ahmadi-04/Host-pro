2d9724a721deb9ca3e82c518841ba44896e93d9a
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-05 19:27:36 +0330

    feat(helper): add helper function
    Des:
    add function to format currency for numbers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const formatCurrency = (
+  amount: number,
+  locale = "en-US",
+  currency = "USD"
+) => {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(amount);
+};
