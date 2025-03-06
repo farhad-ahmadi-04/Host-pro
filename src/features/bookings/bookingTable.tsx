ff2db51633c990133e8125c67166ccdcf2cbc676
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-06 18:58:28 +0330

    feat: call data-table by bookings data

--- src/features/bookings/bookingTable.tsx
+++ src/features/bookings/bookingTable.tsx
@@ -1,6 +1,15 @@
+import { DataTable } from "@/components/ui/table/data-table";
+import LoadingCabins from "../cabins/loadingCabins";
+import { useBookings } from "./useBookings";
+import { bookingsColumns } from "@/features/bookings/bookingsColumns";
+
 function BookingTable() {
+    const { bookings, isLoading, error } = useBookings()
+    if (error) return <p>{error.message}</p>
+    if (isLoading) return <LoadingCabins headNum={5} rowNum={10} />
+
     return (
-        <p>booking table</p>
+        <DataTable columns={bookingsColumns} data={bookings?.data ?? []} columnName="email" />
     );
 }
 
