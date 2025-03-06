0ef7010dc3c9b701d4fdb13e41a87d9f0da9cb6e
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-06 18:59:23 +0330

    feat: add custom hook for get bookings

---
+++ src/features/bookings/useBookings.ts
@@ -0,0 +1,15 @@
+import { getBookings } from "@/services/apiBookings";
+import { useQuery } from "@tanstack/react-query";
+
+export const useBookings = () => {
+  const {
+    data: bookings,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["bookings"],
+    queryFn: getBookings,
+  });
+
+  return { bookings, isLoading, error };
+};
