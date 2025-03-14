d64a042825d41aa7d79f5af97b1d5b98369b1336
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-14 16:10:05 +0330

    feat: show success toast
    Des:
    when cabin delete successfully show toaster

--- src/features/cabins/useDeleteCabin.ts
+++ src/features/cabins/useDeleteCabin.ts
@@ -1,13 +1,14 @@
 import { deleteCabinApi } from "@/services/apiCabins";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 const useDeleteCabin = () => {
   const queryClient = useQueryClient();
 
   const { mutate: deleteCabin, isPending: isDeleting } = useMutation({
     mutationFn: deleteCabinApi,
-    onSuccess: (data) => {
-      console.log(data);
+    onSuccess: () => {
+      toast.success("Cabin successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
