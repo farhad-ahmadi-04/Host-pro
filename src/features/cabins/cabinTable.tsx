65129a0473c21bc2bc725d296571aec511723d6e
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-14 18:01:02 +0330

    add button to add cabin

--- src/features/cabins/cabinTable.tsx
+++ src/features/cabins/cabinTable.tsx
@@ -2,6 +2,7 @@ import { cabinColumns } from "@/features/cabins/cabinColumns";
 import { DataTable } from "@/components/ui/table/data-table";
 import useCabins from "@/features/cabins/useCabins";
 import LoadingCabins from "@/features/cabins/loadingCabins";
+import AddCabinAction from "./addCabinAction";
 
 function CabinTable() {
     const { cabins, error, isLoading, count } = useCabins()
@@ -9,11 +10,14 @@ function CabinTable() {
     if (isLoading) return <LoadingCabins headNum={5} rowNum={5} />
 
     return (
-        <DataTable
-            count={count ?? 0}
-            columns={cabinColumns}
-            data={cabins ?? []}
-            columnName="name" />
+        <>
+            <DataTable
+                count={count ?? 0}
+                columns={cabinColumns}
+                data={cabins ?? []}
+                columnName="name" />
+            <AddCabinAction />
+        </>
     );
 }
 export default CabinTable;
