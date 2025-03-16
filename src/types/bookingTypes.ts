b4dcaa4cba39fc5c43c40a4fe59c9b9e377479bf
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-16 18:19:56 +0330

    update booking type

--- src/types/bookingTypes.ts
+++ src/types/bookingTypes.ts
@@ -1,10 +1,22 @@
 export interface ICabin {
+  created_at: string;
+  description: string;
+  discount: number;
+  id: number;
+  image: string;
+  maxCapacity: number;
   name: string;
+  regularPrice: number;
 }
 
 export interface IGuest {
+  countryFlag: string;
+  created_at: string;
   email: string;
   fullName: string;
+  id: number;
+  nationalID: string;
+  nationality: string;
 }
 
 export interface IBooking {
