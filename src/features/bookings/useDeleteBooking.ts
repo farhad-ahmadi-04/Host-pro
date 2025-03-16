c366ac7722f1a4914bf9960e0470d026282f1064
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-16 13:30:54 +0330

    feat: add mutation for delete cabin
    Des:
    create custom hook for delete booking by ID

---
+++ src/features/bookings/useDeleteBooking.ts
@@ -0,0 +1,26 @@
+import { deleteBookingApi } from "@/services/apiBookings";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+const useDeleteBooking = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteBooking, isPending: isDeleting } = useMutation({
+    mutationFn: (id: number) => deleteBookingApi(id),
+    onSuccess: () => {
+      toast.success("Booking successfully deleted");
+
+      queryClient.invalidateQueries({
+        queryKey: ["bookings"],
+      });
+    },
+
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+
+  return { deleteBooking, isDeleting };
+};
+export default useDeleteBooking;
