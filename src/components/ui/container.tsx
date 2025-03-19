8b4f23d92b890405c144aa5b402c86306320d516
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-19 19:54:04 +0330

    refactor: set optional prop for className

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-function Container({ className, children }: { className: string, children: React.ReactNode }) {
+function Container({ className, children }: { className?: string, children: React.ReactNode }) {
     return (
         <div className={cn("w-full max-w-[1400px]", className)}>
             {children}
