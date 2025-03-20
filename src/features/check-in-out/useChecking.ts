5cc757f0a7439690b69ea1d23da14753ea83666c
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-20 17:22:02 +0330

    refactor: add query client to hook

--- src/features/check-in-out/useChecking.ts
+++ src/features/check-in-out/useChecking.ts
@@ -1,8 +1,10 @@
 import { UpdateBooking } from "@/services/apiBookings";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 const useChecking = () => {
+  const queryClient = useQueryClient();
+
   const { mutate: checkin, isPending: isChecking } = useMutation({
     mutationFn: ({
       bookingId,
@@ -22,6 +24,10 @@ const useChecking = () => {
       }),
     onSuccess: () => {
       toast.success("Check-in successful");
+
+      queryClient.invalidateQueries({
+        refetchType: "active",
+      });
     },
     onError: () => {
       toast.error("Failed to check-in");
