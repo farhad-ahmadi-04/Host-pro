737a07c60537ab3914f28da8a8249f9788757e58
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-24 21:07:29 +0430

    feat: add signup form
    Des:
    create signup form by shadcn form and validate with zod
    and call it to user page

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,5 +1,17 @@
+import Section from "@/components/ui/section";
+import SignupForm from "@/features/authentication/signupForm";
+
 function Users() {
-    return (<h1>Users</h1>);
+    return (
+        <Section>
+            <div>
+                <h1 className="text-lg md:text-2xl lg:text-4xl font-semibold">Create a new user</h1>
+            </div>
+            <div className="lg:w-8/12">
+                <SignupForm />
+            </div>
+        </Section >
+    );
 }
 
 export default Users;
