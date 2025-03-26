0769a9e66a2ce722f9fe7f49439a5a4a6029ff30
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-26 22:03:34 +0430

    call settingsForm component

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,5 +1,15 @@
+import Section from "@/components/ui/section";
+import UpdateSettingsForm from "@/features/settings/updateSettingsForm";
+
 function Settings() {
-    return (<h1>Settings</h1>);
+    return (<Section>
+        <div>
+            <h1 className="text-lg md:text-2xl lg:text-4xl font-semibold">All bookings</h1>
+        </div>
+        <div className="lg:w-8/12">
+            <UpdateSettingsForm />
+        </div>
+    </Section>);
 }
 
 export default Settings;
