7b4336f7612a10c56307a8e7090824a00e5e3064
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-27 23:02:45 +0430

    feat: add function to update user data

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,5 +1,5 @@
 import { IUserLogin, IUserSignup } from "@/types/authTypes";
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 // login
 export const userLogin = async ({ email, password }: IUserLogin) => {
@@ -49,4 +49,47 @@ export const signupApi = async ({ email, password, fullName }: IUserSignup) => {
   if (error) throw new Error(error.message);
 
   return data;
+};
+
+// Update the user profile
+export const updateCurrentUser = async ({
+  avatar,
+  fullName,
+  password,
+}: {
+  avatar?: File | null;
+  fullName?: string;
+  password?: string;
+}) => {
+  // 1. update password or fullName
+  const updateData: { data?: { fullName: string }; password?: string } = {};
+  if (password) updateData.password = password;
+
+  if (fullName) updateData.data = { fullName };
+
+  if (Object.keys(updateData).length === 0) {
+    throw new Error("No update parameter provided.");
+  }
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  // 2.upload avatar image
+  const fileName = `avatar-${avatar.lastModified}-${Math.random()}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. update avatar in the user
+  const { data: updateUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  console.log(supabaseUrl);
+
+  if (error2) throw new Error(error2.message);
+  return updateUser;
 };
