fd884b21c4253ed7edbcd73bd1f50e5b0f8e85c0
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-28 23:08:08 +0430

    feat: add dashboard box component

---
+++ src/features/dashbord/DashboardBox.tsx
@@ -0,0 +1,13 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+function DashboardBox() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Dashboard box title</CardTitle>
+            </CardHeader>
+        </Card>
+    );
+}
+
+export default DashboardBox;
