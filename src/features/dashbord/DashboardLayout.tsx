951875863bb6eda214d85709c3c6295344d68a1f
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-29 21:21:09 +0430

    feat: add filter for get last days to dashboard page

--- src/features/dashbord/DashboardLayout.tsx
+++ src/features/dashbord/DashboardLayout.tsx
@@ -1,13 +1,19 @@
 import Container from "@/components/ui/container";
 import Section from "@/components/ui/section";
 import DashboardBox from "./DashboardBox";
+import Filter from "@/components/ui/filter";
 
 function DashboardLayout() {
     return (
         <Section>
-            <Container>
-                <div>
+            <Container className="space-y-5">
+                <div className="flex justify-between">
                     <h1 className="text-lg md:text-2xl lg:text-4xl font-semibold">Dashboard</h1>
+                    <Filter filterField="last" options={[
+                        { value: "7", label: "Last 7 days" },
+                        { value: "30", label: "Last 30 days" },
+                        { value: "90", label: "Last 90 days" },
+                    ]} />
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <DashboardBox />
