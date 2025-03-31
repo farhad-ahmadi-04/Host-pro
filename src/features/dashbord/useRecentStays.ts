8c2dac8c3421ac41944c8a4d4cacb281a644f24f
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-31 13:41:47 +0430

    feat: add custom hook to get recent stats

---
+++ src/features/dashbord/useRecentStays.ts
@@ -0,0 +1,23 @@
+import { getStaysAfterDate } from "@/services/apiBookings";
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+
+export function useRecentStays() {
+  const [searchParams] = useSearchParams();
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+  const queryDays = subDays(new Date(), numDays).toISOString();
+
+  const { data: stays, isLoading } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDays),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "checked-in" || stay.status === "checked-out"
+  );
+
+  return { stays, isLoading, confirmedStays, numDays };
+}
