18161bb5784933f337595a8640aeb3ecdda7104c
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-03-31 13:46:06 +0430

    add logic to and set value to stat

--- src/features/dashbord/stats.tsx
+++ src/features/dashbord/stats.tsx
@@ -1,46 +1,54 @@
-import { IBookingsState } from "@/types/bookingTypes";
+import { IBooking, IBookingsState } from "@/types/bookingTypes";
 import Stat from "./stat";
 import { Briefcase, CalendarDays, ChartColumnIncreasing, DollarSign } from "lucide-react";
+import { formatCurrency } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
 
-function Stats({ bookings = [], cabins = 0 }: {
+function Stats({ bookings = [], cabins = 0, confirmedStays, numDays, loading }: {
     bookings?: IBookingsState[];
-    cabins?: number
+    cabins?: number;
+    confirmedStays: IBooking[];
+    numDays: number;
+    loading: boolean
 }) {
-    console.log(cabins);
 
     // 1.
     const numBookings = String(bookings.length)
 
-    // //   4.
-    // const occupation =
-    //     confirmedStays?.reduce((acc, cur) => acc + cur.numNights, 0) /
-    //     (numDays * cabinCount?.length);
-    // // num checked in nights / all available nights ()num days * num cabins)
+    // 2.
+    const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0)
 
+    // 3.
+    const checkins = confirmedStays?.length;
+
+    //   4.
+    const occupation =
+        confirmedStays?.reduce((acc, cur) => acc + cur.numNights, 0) /
+        (numDays * cabins);
+    // num checked in nights / all available nights ()num days * num cabins)
+
+    if (loading) return <Skeleton className="h-10 md:h-20" />
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <>
             <Stat
                 title={"Bookings"}
-                // color={"blue"}
                 icon={<Briefcase />}
                 value={numBookings} />
             <Stat
                 title={"Sales"}
-                // color={"blue"}
                 icon={<DollarSign />}
-                value={numBookings} />
+                value={formatCurrency(sales)} />
             <Stat
                 title={"Check ins"}
-                // color={"blue"}
                 icon={<CalendarDays />}
-                value={numBookings} />
+                value={checkins.toString()} />
             <Stat
                 title={"Occupancy rate"}
-                // color={"blue"}
                 icon={<ChartColumnIncreasing />}
-                value={numBookings} />
-        </div>
+                value={Math.round(occupation * 100) + "%"} />
+        </>
+
     );
 }
 
