5f64fec1d827790a01282e0455b778a3f0c65a92
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-04-03 19:36:18 +0430

    feat: add function to get today activity
    Des:
    Activity means that there is a check in or a check out today

--- src/services/apiBookings.ts
+++ src/services/apiBookings.ts
@@ -97,12 +97,35 @@ export async function getStaysAfterDate(date: string) {
     // .select('*')
     .select("*, guests(fullName)")
     .gte("startDate", date)
-    .lte("startDate", getToday({ end: false }));
+    .lte("startDate", getToday({}));
 
   if (error) {
     console.error(error);
     throw new Error("Bookings could not get loaded");
   }
 
+  return data;
+}
+
+// Activity means that there is a check in or a check out today
+export async function getStaysTodayActivity(): Promise<IBooking[]> {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, guests(fullName, nationality, countryFlag)")
+    .or(
+      `and(status.eq.unconfirmed,startDate.eq.${getToday(
+        {}
+      )}),and(status.eq.checked-in,endDate.eq.${getToday({})})`
+    )
+    .order("created_at");
+
+  // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
+  // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
+  // (stay.status === 'checked-in' && isToday(new Date(stay.endDate)))
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
   return data;
 }
