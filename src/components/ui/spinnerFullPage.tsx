f7318db655c26dc2f8257bf229e7e7a6479f05c4
farhad-ahmadi-04 <farhad.13ahmadi.83@gmail.com>
2025-04-06 21:37:15 +0430

    feat: add spinner component for page

---
+++ src/components/ui/spinnerFullPage.tsx
@@ -0,0 +1,9 @@
+function SpinnerFullPage() {
+    return (
+        <div className="w-dvw h-dvh flex justify-center items-center">
+            <span className="loader"></span>
+        </div>
+    );
+}
+
+export default SpinnerFullPage;
